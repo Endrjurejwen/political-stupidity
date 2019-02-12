004d3ee0296e0b4f58d39ea8a6be95080947c2e8
Andrzej Kruk <endrjurejwen@gmail.com>
2019-02-12 15:48:25 +0100

    added navigation test

---
+++ src/App.test.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { fireEvent } from 'react-testing-library';
+import { renderWithRouter } from 'utils';
+import App from './App';
+
+describe('<App />', () => {
+  test('should navigate corectly', () => {
+    const {
+      queryByTestId,
+      getByTestId,
+      getByText,
+      queryByText
+    } = renderWithRouter(<App />);
+
+    const homeLink = queryByText(/strona główna/i);
+    const aboutLink = queryByText(/idea/i);
+    const termsLink = queryByText(/regulamin/i);
+    const logoutLink = queryByText(/wyloguj się/i);
+    const lefClick = { button: 0 };
+
+    expect(queryByText(/dashboard page/i)).toBeInTheDocument();
+    expect(queryByText(/strona o pomyśle/i)).not.toBeInTheDocument();
+    expect(homeLink).toHaveClass('active');
+    [aboutLink, termsLink, logoutLink].map(link =>
+      expect(link).not.toHaveClass('active')
+    );
+
+    fireEvent.click(aboutLink, lefClick);
+    expect(queryByText(/strona o pomyśle/i)).toBeInTheDocument();
+    expect(queryByText(/dashboard page/i)).not.toBeInTheDocument();
+    expect(getByTestId('sideDrawer')).toHaveStyle(
+      'transform: translateY(-30vh)'
+    );
+    expect(queryByTestId('backdrop')).not.toBeInTheDocument();
+    expect(aboutLink).toHaveClass('active');
+    [homeLink, termsLink, logoutLink].map(link =>
+      expect(link).not.toHaveClass('active')
+    );
+
+    fireEvent.click(getByText(/dodaj cytat/i));
+    expect(queryByText(/strona o pomyśle/i)).not.toBeInTheDocument();
+    expect(queryByText(/create quotation/i)).toBeInTheDocument();
+    [homeLink, aboutLink, termsLink, logoutLink].map(link =>
+      expect(link).not.toHaveClass('active')
+    );
+  });
+});
