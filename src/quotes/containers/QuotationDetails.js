ccabd24cf09d1f60036e902bf8a5a0e6bc0eb645
Andrzej Kruk <endrjurejwen@gmail.com>
2019-03-13 13:59:04 +0100

    added like and delete functionality to QuotationDetails container

--- src/quotes/containers/QuotationDetails.js
+++ src/quotes/containers/QuotationDetails.js
@@ -1,83 +1,62 @@
-import React from 'react';
+import React, { Component } from 'react';
 import styled from 'styled-components';
 import Quotation from 'quotes/components/Quotation';
 import CommmentsList from 'comments/components/CommentsList';
-import { compose } from 'redux';
+import { compose, bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import { firestoreConnect } from 'react-redux-firebase';
 import { Spinner } from 'common';
-
 import { H2 } from 'elements';
 import { spacing } from 'utils';
+import {
+  addToFavorites,
+  removeFromFavorites,
+  deleteQuotation
+} from 'quotes/actions';
 
-// const FAKE_QUOTES = [
-//   {
-//     body:
-//       'Wtedy, kiedy dinozaury jeszcze były, a ludzie nie mieli żadnych strzelb, nie mieli żadnej broni nowoczesnej, która pozwoliłaby ich zabić.',
-//     author: 'Ewa Kopacz',
-//     user: 'Biedny Obywatel',
-//     likes: 34,
-//     timestamp: '12-02-2019',
-//     id: 'gfh5465464654g4',
-//     comments: [
-//       {
-//         body: 'to się popisała Pani Premier :)',
-//         likes: 25,
-//         user: 'Jaś Gamoń',
-//         id: 't53regfdgdf',
-//         timestamp: '14-02-2019'
-//       },
-//       {
-//         body: 'Hahahahaha, nie wierzę',
-//         likes: 7,
-//         user: 'Inny Gamoń',
-//         id: 'sdfdsf45tgdgf',
-//         timestamp: '14-02-2019'
-//       }
-//     ]
-//   },
-//   {
-//     body:
-//       'Założenie przypadkowego powstania życia niczym się nie różni od rozumowania, że tornado przechodzące przez samolotowe złomowisko może złożyć boeinga 747 gotowego do lotu.',
-//     author: 'Maciej Giertych',
-//     user: 'Bartłomiej Kowalski',
-//     likes: 23,
-//     timestamp: '14-02-2019',
-//     id: 'sdfsdgfgfdgfdg',
-//     comments: [
-//       {
-//         body: 'Jego to ewolucja nie rusza',
-//         likes: 12,
-//         user: 'Jan Nowak',
-//         id: 'sgfsg45gsdgdf',
-//         timestamp: '14-02-2019'
-//       },
-//       {
-//         body: 'Hahahahaha, nie no, ten to wymyślił',
-//         likes: 19,
-//         user: 'Halina Konopna',
-//         id: 'dsgdfgdff44gdfg',
-//         timestamp: '14-02-2019'
-//       }
-//     ]
-//   }
-// ];
+class QuotationDetails extends Component {
+  toFavoritesHandler = () => {
+    const {
+      match,
+      quotation,
+      auth,
+      addToFavorites,
+      removeFromFavorites
+    } = this.props;
 
-const quotationDetails = ({ quotation }) => {
-  if (!quotation) {
-    return <Spinner />;
-  }
+    if (!(auth.uid in quotation.likes)) {
+      addToFavorites(match.params.id);
+    }
+    if (auth.uid in quotation.likes) {
+      removeFromFavorites(match.params.id);
+    }
+  };
 
-  if (quotation) {
+  deleteQuotationHandler = () => {
+    const { match, history, deleteQuotation } = this.props;
+    deleteQuotation(match.params.id);
+    history.push('/home');
+  };
+
+  render() {
+    const { quotation, auth } = this.props;
+    if (!quotation) {
+      return <Spinner />;
+    }
     return (
       <>
-        <Quotation quotation={quotation} />
+        <Quotation
+          quotation={quotation}
+          userId={auth.uid}
+          likeClick={this.toFavoritesHandler}
+          deleteClick={this.deleteQuotationHandler}
+        />
         <Title>Komentarze</Title>
         <CommmentsList comments={quotation.comments} />
       </>
     );
   }
-};
+}
 
 const mapStateToProps = (state, ownProps) => {
   const { id } = ownProps.match.params;
@@ -85,16 +64,35 @@ const mapStateToProps = (state, ownProps) => {
   const quotation = quotes ? quotes[id] : null;
 
   return {
-    quotation
+    quotation,
+    auth: state.firebase.auth
   };
 };
 
+const mapDispatchToProps = dispatch =>
+  bindActionCreators(
+    {
+      addToFavorites,
+      removeFromFavorites,
+      deleteQuotation
+    },
+    dispatch
+  );
+
 export default compose(
-  connect(mapStateToProps),
-  firestoreConnect([{ collection: 'quotes' }])
-)(quotationDetails);
+  connect(
+    mapStateToProps,
+    mapDispatchToProps
+  ),
+  firestoreConnect(props => [{ collection: 'quotes' }])
+)(QuotationDetails);
 
 const Title = styled(H2)`
   text-align: center;
   margin-bottom: ${spacing[3]};
 `;
+
+// export default compose(
+//   connect(mapStateToProps),
+//   firestoreConnect([{ collection: 'quotes' }])
+// )(QuotationDetails);
