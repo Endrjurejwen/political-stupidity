cb132dca1d72044ca115afdee02a62f304376f59
Andrzej Kruk <endrjurejwen@gmail.com>
2019-05-30 17:35:17 +0200

    added locale to moment

--- src/app/common/info/Data/Data.js
+++ src/app/common/info/Data/Data.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import { shape } from 'prop-types';
 import moment from 'moment';
+import 'moment/locale/pl';
 
 import * as S from './style';
+
+moment.locale('pl');
 
 const data = ({ dataNumber }) => (
   <S.Data data-testid="quotation-timestamp">
